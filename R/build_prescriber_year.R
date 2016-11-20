c91a77a68c06abd8dfc96cdf1d24ee0edc7bde3b
Thomas U. Aguilar <taguilar87@gmail.com>
2016-11-20 11:42:23 -0500

    paths for raw source reader method

--- R/build_prescriber_year.R
+++ R/build_prescriber_year.R
@@ -5,7 +5,7 @@ build_prescriber_year <- R6::R6Class(
   public = list(
     
     year = NULL,
-    source_file_dir = '~/Dropbox/physician_payments/',
+    source_file_dir = '~/Dropbox/physician_payments/raw_source_data/',
     drug_folder = 'PartD_Prescriber_PUF_NPI_DRUG',
     phys_folder = 'PartD_Prescriber_PUF_NPI',
     partd_drug_source = dplyr::data_frame(),
@@ -23,11 +23,13 @@ build_prescriber_year <- R6::R6Class(
     
     read_source_tables = function() {
       self$partd_drug_source = readr::read_delim(
-        paste0(self$source_file_dir, self$drug_folder, '/', self$year, '/PartD_Prescriber_PUF_NPI_Drug.txt'),
+        paste0(self$source_file_dir, self$drug_folder, '/', self$year,
+               '/PartD_Prescriber_PUF_NPI_Drug.txt'),
         delim = '\t')
       
       self$partd_phys_source = readr::read_delim(
-        paste0(self$source_file_dir, self$phys_folder, '/', self$year, '/PartD_Prescriber_PUF_NPI.txt'),
+        paste0(self$source_file_dir, self$phys_folder, '/', self$year, 
+               '/PartD_Prescriber_PUF_NPI.txt'),
         delim = '\t')
     },
     
