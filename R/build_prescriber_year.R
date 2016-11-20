e0916546cc554bc3bc3cd1df85e1d6d4f8d3b430
Thomas U. Aguilar <taguilar87@gmail.com>
2016-11-20 00:08:26 -0500

    adding paths to raw source files

--- R/build_prescriber_year.R
+++ R/build_prescriber_year.R
@@ -6,8 +6,11 @@ build_prescriber_year <- R6::R6Class(
     
     year = NULL,
     source_file_dir = '~/Dropbox/physician_payments/',
+    drug_folder = 'PartD_Prescriber_PUF_NPI_DRUG',
+    phys_foler = 'PartD_Prescriber_PUF_NPI',
     
-    initialize = function(yr = year) {
+    
+    initialize = function(yr = '2014') {
       self$year <- yr
     },
     
