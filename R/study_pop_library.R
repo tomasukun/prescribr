72ac6c549f89516263d131be19ab4846e75c93f8
Thomas U. Aguilar <taguilar87@gmail.com>
2016-11-26 22:39:44 -0500

    calling spec specialty categories in study pop library class

--- R/study_pop_library.R
+++ R/study_pop_library.R
@@ -14,8 +14,12 @@ study_pop_library <- R6::R6Class(
     processed_file_dir = '~/Dropbox/physician_payments/processed_source_data/',
     shared_docs_dir = '~/Dropbox/OP2 Documents/',
     
+    doc_specialty_categories = suppressMessages(
+      readr::read_csv('spec/partd_doc_specialty_tabulation_2014.csv')),
+    
     study_pop = list(partd_docs = NULL,
                      openpay_docs = NULL,
+                     partd_specialties_keep = NULL,
                      partd_us_docs = NULL,
                      valid_brand_docs = NULL,
                      phys_comp_docs = NULL,
@@ -23,10 +27,10 @@ study_pop_library <- R6::R6Class(
                      med_grad_exclude_docs = NULL,
                      unq_match_crit_op_docs = NULL,
                      final_study_docs = NULL),
+    
+    
     exclusion_criteria = list(
       doc = 'M.?D.?|D.?O.?',
-      specialty_exclusion = readr::read_csv(self$shared_docs_dir,
-                                            'partd_doc_specialty_tabulation_2014.csv'),
       claim_count = 100,
       states = c(state.abb, 'DC'),
       grad_year_limits = c(1910, 2013)
