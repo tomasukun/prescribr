28aee4d26861bf33a17b8f54e9e19152285c4ce8
Thomas U. Aguilar <taguilar87@gmail.com>
2016-12-19 22:44:20 -0500

    adding per bene target prescribing

--- R/build_changer_data.R
+++ R/build_changer_data.R
@@ -37,7 +37,8 @@ build_changer_data <- R6::R6Class(
         select(NPI, 
                base_year_class_claims = total_class_claims,
                base_year_target_claims = total_target_claims, 
-               base_year_payments = total_target_payment_number) %>% 
+               base_year_payments = total_target_payment_number,
+               base_year_bene_count = doc_bene_count) %>% 
         mutate(base_year = year)
     },
     
@@ -49,7 +50,8 @@ build_changer_data <- R6::R6Class(
                doc_specialty,
                change_year_class_claims = total_class_claims,
                change_year_target_claims = total_target_claims, 
-               change_year_payments = total_target_payment_number) %>% 
+               change_year_payments = total_target_payment_number,
+               change_year_bene_count = doc_bene_count) %>% 
         mutate(change_year = year)
     },
     
@@ -71,21 +73,29 @@ build_changer_data <- R6::R6Class(
         select(-base_year_payments, -change_year_payments) %>% 
         tidyr::gather(key = 'year_vars', value = 'year', base_year, change_year,
                       -base_year_class_claims, -base_year_target_claims,
-                      -change_year_class_claims, -change_year_target_claims) %>%
+                      -change_year_class_claims, -change_year_target_claims, 
+                      -base_year_bene_count, -change_year_bene_count) %>%
         group_by(NPI, year, paid_group) %>%
         summarise(target_claims = ifelse(year == self$base_year,
                                         base_year_target_claims,
                                         change_year_target_claims),
                   class_claims = ifelse(year == self$base_year,
                                         base_year_class_claims,
-                                        change_year_class_claims)) %>%
+                                        change_year_class_claims),
+                  bene_count = ifelse(year == self$base_year,
+                                      base_year_bene_count,
+                                      change_year_bene_count)) %>%
         ungroup() %>%
+        mutate(
+          target_per_bene = target_claims/(bene_count/100)
+        ) %>% 
         group_by(year, paid_group) %>%
         summarise(
           group_count = n(),
           total_claims = sum(target_claims, na.rm = TRUE),
           total_class_claims = sum(class_claims, na.rm = TRUE),
-          mean_prescribing_rate = round(mean(target_claims), 1)
+          mean_prescribing_rate = round(mean(target_claims), 1),
+          mean_target_per_bene = round(mean(target_per_bene), 1)
           #p10_prescribing_rate  = quantile(target_claims, probs = 0.1),
           #p25_prescribing_rate  = quantile(target_claims, probs = 0.25),
           #p75_prescribing_rate  = quantile(target_claims, probs = 0.75),
@@ -109,8 +119,8 @@ build_changer_data <- R6::R6Class(
       self$tidy_combined_data()
       if(self$create_figure == TRUE) {
         figure_data <- self$tidy_data %>%
-          select(year, paid_group, mean_prescribing_rate) %>%
-          tidyr::spread(year, mean_prescribing_rate) %>%
+          select(year, paid_group, mean_target_per_bene) %>%
+          tidyr::spread(year, mean_target_per_bene) %>%
           mutate(percent_diff = round(100*(`2014` - `2013`)/`2013`))
         # figure_data <- self$tidy_data %>%
         #   select(year, paid_group, target_rate) %>%
@@ -122,7 +132,7 @@ build_changer_data <- R6::R6Class(
           increase = str_c(figure_data$percent_diff, '%')
         )
         figure1 <- ggplot2::ggplot(self$tidy_data, 
-                                   aes(x = year, y = mean_prescribing_rate, group = paid_group, 
+                                   aes(x = year, y = mean_target_per_bene, group = paid_group, 
                                        colour = paid_group)) +
           geom_line(size = 1.12) +
           geom_point(size = 5.2, shape = 21, fill = "white") + 
@@ -139,12 +149,12 @@ build_changer_data <- R6::R6Class(
                                          'Change Year Meal' = '#C70039',
                                          'Both Year Meals' = '#581845')
                               ) +
-          ylab("Diclofenac (Flector \U00AE) Prescribing Rate, # per Physician \n") + 
+          ylab("Difluprednate (Durezol \U00AE) Prescribing Rate per Beneficiary, # per 100 Beneficiaries \n") + 
           labs(color = "Meal Payment Group") + 
           theme(legend.text = element_text(size = 18)) + 
           theme(legend.title = element_text(size = 18)) + 
           theme(legend.key.size = unit(1.5, "cm")) + 
-          scale_y_continuous(limits = c(0, 1.5), breaks = seq(0, 1.5, 0.1), expand = c(0,0)) +
+          scale_y_continuous(limits = c(4, 22), breaks = seq(4, 22, 2), expand = c(0,0)) +
           theme(axis.text = element_text(face = "bold", size = 17, colour = "black")) +
           theme(panel.background = element_rect(colour = "white", fill = "white")) +
           theme(axis.line = element_line(colour = "black")) +
@@ -153,7 +163,7 @@ build_changer_data <- R6::R6Class(
           theme(panel.grid.major.y = element_line(colour = "grey90")) +
           theme(axis.ticks = element_line(colour = "black")) + 
           theme(axis.title = element_text(hjust = 0.5)) +
-          ggtitle("Flector") +
+          ggtitle("Durezol") +
           theme(title = element_text( size = 18, color = "black", hjust = 0.5, face = "bold"))
         browser()
         # jpeg(filename = paste0(self$shared_docs_dir, 'figure1_changer_analysis_', self$drug_class, '.jpeg'), 
