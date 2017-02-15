e3a515311f4f897c6d7c6e53c1e4b7a4d2072423
Thomas U. Aguilar <taguilar87@gmail.com>
2017-02-15 00:55:14 -0500

    fleshing out analysis class, mostly adding sandwich estimators for robust SE estimation

--- R/build_analysis.R
+++ R/build_analysis.R
@@ -23,9 +23,7 @@ build_analysis <- R6::R6Class(
         mutate(spec_cat = if_else(doc_specialty %in% c('Internal Medicine', 'Hospitalist'),
                                   "Internal Medicine",
                                   if_else(doc_specialty %in% c('Family Practice', 'Family Medicine', 'General Practice'),
-                                          'Family Medicine', 
-                                          if_else(!(doc_specialty %in% c("Family Practice", "Family Medicine", "General Practice",
-                                                                         "Internal Medicine", "Hospitalist"))))),
+                                          'Family Medicine', "Other")),
                grad_year_cat = if_else((2013 - doc_grad_year) <= 5, 1,
                                        if_else((2013 - doc_grad_year) > 20, 3, 2)),
                total_vol_per100 = doc_total_claims/100,
@@ -35,17 +33,37 @@ build_analysis <- R6::R6Class(
         )
     },
     
-    build_analysis = function() {
+    build_model = function() {
       if(self$type == 'did') {
+        cat('Difference-in-Difference Analysis (DID) \n')
         did_glm <- glm(cbind(target_claims, class_claims) ~ factor(paid) + factor(year) + did + 
                          factor(spec_cat) + factor(grad_year_cat) + total_vol_per100,
                        data = self$analysis_data, family = 'binomial')
         summary(did_glm)
+        cat('Odds Ratios \n')
+        exp(cbind(OR = coef(did_glm), confint(did_glm)))
+        
+        cat('DID with Robust Standard Errors \n')
+        cov <- sandwich::vcovHC(did_glm, type = "HC0")
+        std_err <- sqrt(diag(cov))
+        estimation <- data_frame(
+          coefficients = names(did_glm$coefficients),
+          estimate = coef(did_glm), 
+          "RSE" = std_err,
+          z = (coef(did_glm)/std_err),
+          "P" = 2 * pnorm(abs(coef(did_glm)/std_err), lower.tail = FALSE),
+          LL = coef(did_glm) - qnorm(0.975)  * std_err,
+          UL = coef(did_glm) + qnorm(0.975)  * std_err
+        )
+        estimation
+        cat('Odds Ratios \n')
+        or_estimation <- estimation %>% 
+          select(coefficients, estimate, LL, UL) %>% 
+          mutate_if(is.numeric, exp)
+        or_estimation
       } else {
         cat(sprintf('%s is not a supported analysis method \n\n analysis data not created', self$type))
       }
     }
-    
-    
   )
 )
