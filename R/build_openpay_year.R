38cbe795d69c228e547a4aa7e4ddb0814e92ef8f
Thomas U. Aguilar <taguilar87@gmail.com>
2018-11-17 22:51:00 -0500

    Remove whitespace and trailing spaces

--- R/build_openpay_year.R
+++ R/build_openpay_year.R
@@ -6,23 +6,23 @@ build_openpay_year <- R6::R6Class(
   'build_openpay_year',
   inherit = study_pop_library,
   public = list(
-    
+
     year = NULL,
     openpay_folder = 'Open_Payments',
     openpay_source = dplyr::data_frame(),
     openpay_docs = dplyr::data_frame(),
-    
+
     initialize = function(yr = '2014') {
       self$year <- yr
     },
-    
+
     read_source_tables = function() {
       self$openpay_source <- readr::read_csv(
         paste0(self$source_file_dir, self$openpay_folder, '/',
                self$year, '/OP_DTL_GNRL_PGYR', self$year, '_P06302016.csv')
-        ) %>% 
+        ) %>%
         filter(Dispute_Status_for_Publication == 'No',
-               Third_Party_Payment_Recipient_Indicator == 'No Third Party Payment') %>% 
+               Third_Party_Payment_Recipient_Indicator == 'No Third Party Payment') %>%
         select(doc_id = Physician_Profile_ID,
                doc_last_name = Physician_Last_Name,
                doc_first_name = Physician_First_Name,
@@ -45,23 +45,23 @@ build_openpay_year <- R6::R6Class(
                payment_device_2 = Name_of_Associated_Covered_Device_or_Medical_Supply2,
                payment_device_3 = Name_of_Associated_Covered_Device_or_Medical_Supply3,
                payment_device_4 = Name_of_Associated_Covered_Device_or_Medical_Supply4,
-               payment_device_5 = Name_of_Associated_Covered_Device_or_Medical_Supply5) %>% 
+               payment_device_5 = Name_of_Associated_Covered_Device_or_Medical_Supply5) %>%
         mutate(doc_mi = str_sub(doc_mi, 1, 1),
                doc_zip = str_sub(doc_zip, 1, 5),
                payment_date = lubridate::mdy(payment_date))
     },
-    
+
     process_source_tables = function() {
       self$openpay_docs <- self$openpay_source %>%
-        group_by(doc_id) %>% 
-        mutate(total_payment_dollars = sum(payment_dollars, na.rm = TRUE)) %>% 
+        group_by(doc_id) %>%
+        mutate(total_payment_dollars = sum(payment_dollars, na.rm = TRUE)) %>%
         select(doc_id, doc_last_name, doc_first_name, doc_city, doc_state, doc_zip,
-               total_payment_dollars) %>% 
+               total_payment_dollars) %>%
         distinct(doc_id, .keep_all = TRUE)
       study_pop <- Kmisc::kLoad('data/study_pop.rData')
       study_pop[[paste0('study_', self$year)]]$openpay_docs <- nrow(self$openpay_docs)
     },
-    
+
     save_processed_tables = function() {
       save(study_pop, file = 'data/study_pop.rData')
       openpay <- self$openpay_source
@@ -69,13 +69,13 @@ build_openpay_year <- R6::R6Class(
       save(openpay, file = paste0(self$processed_file_dir, self$year,'/open_payments.rData'))
       save(openpay_docs, file = paste0(self$processed_file_dir, self$year,'/open_payments_docs.rData'))
     },
-    
+
     build_tables = function() {
       self$read_source_tables()
       self$process_source_tables()
       self$save_processed_tables()
     }
-    
+
   )
 )
-    
+
